237e8bfc508ec086c677b072ae28fb32f14e51c5
ct-bess <bessellc@gmail.com>
2018-10-10 14:50:08 -0500

    UX: Button integrated, make it do something

--- frontend-uSite/usite/src/App.js
+++ frontend-uSite/usite/src/App.js
@@ -6,6 +6,9 @@ import './App.css';
 // - This guy defines a specific textarea and display
 class Textbox extends Component {
 
+  // Each render method allows the use of ONE outer </div>
+  // - Multiple inner </div> is allowed though
+  // - If you are rendering 1 HTML element, you can just return that element
   render() {
     return(
       <div>
@@ -13,7 +16,27 @@ class Textbox extends Component {
           onChange = {this.props.onChange}
           defaultValue = {this.props.value}
         />
-        <p>{this.props.value}</p>
+        <p>This is the output: {this.props.value}</p>
+        <div>
+          Also checkout the console log for more info.
+        </div>
+      </div>
+    );
+  }
+
+}
+
+class Submitbutton extends Component {
+
+  render() {
+    return(
+      <div>
+        <button
+          className = "Submitbutton"
+          onClick = {this.props.onClick}
+        >
+          Submit
+        </button>
       </div>
     );
   }
@@ -31,6 +54,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       value: "YES",
     };
@@ -42,6 +66,12 @@ class App extends Component {
     console.log( "State Change, new state value = " + e.target.value );
   }
 
+  handleClick() {
+    const value = this.state.value;
+    console.log( "Submit button clicked, captured value = " + value );
+    // Here we can send the value somewhere when the user clicks the button
+  }
+
   // Render & return methods are a staple in each component
   // - They must exist
   render() {
@@ -49,16 +79,28 @@ class App extends Component {
     // We can define typical JS stuff here
     // - e.g. variables, helper functions
 
+    const value = this.state.value;
+
     // The return is JSX language (Babel)
     // - Babel is a preprocessor JS language that is converted into plain JS
     // - Babel just makes our job easier
     return (
       <div>
-        <Textbox
-          onChange = {this.handleChange}
-          defaultValue = {this.state.value}
-          value = {this.state.value}
-        />
+        <div className = "Header">
+          Look magic! {value}
+        </div>
+        <div>
+          <Textbox
+            onChange = {this.handleChange}
+            defaultValue = {this.state.value}
+            value = {this.state.value}
+          />
+        </div>
+        <div>
+          <Submitbutton
+            onClick = { () => this.handleClick() }
+          />
+        </div>
       </div>
     );
   }
